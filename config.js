9ca213f5e9f849cac09251fce0c91e361a0f0744
jass-boop <jasonmason129n90p@gmail.com>
2025-01-22 22:03:46 +0300

    Update config.js
    Yes

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "mvwSRQTT#eJqF3fqlSj01DeYIJhpqm4HXRKYxfYuwaslkEtnSElI",
     CAPTION: process.env.CAPTION || "*ᴘᴏᴡᴅᴇʀᴇᴅ ʙʏ ᴡᴀʟᴋ-ᴅʀᴏᴠᴇ*",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
